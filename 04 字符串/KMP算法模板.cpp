ac0897333eec2aedaed9f3202ece22fff83d365b
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-11-28 11:59:47 +0800

    Update KMP算法模板.cpp

--- 04 字符串/KMP算法模板.cpp
+++ 04 字符串/KMP算法模板.cpp
@@ -8,7 +8,7 @@ void GetNext(int *nxt, int *nxtval, string b)
 {
     nxt[0] = nxtval[0] = -1;
     int lb = b.length();
-    int i = 0, j = -1; //计算nxt数组将指针初始化为0和-1！！！
+    int i = 0, j = -1; //计算nxt数组将指针初始化为0和-1
     while (i < lb)
         if (j == -1 || b[i] == b[j])
             nxt[++i] = ++j, nxtval[i] = j, nxtval[i] = (b[i] == b[j] ? nxtval[j] : nxtval[i]); //加上对nxt[i]的优化成为nxtval数组
@@ -31,9 +31,7 @@ void KMP(int *nxt, string a, string b)
         else //不匹配就将模式串指针回溯，从而从模式串更靠前的位置开始匹配
             j = nxt[j];
         if (j == lb)
-            cout << "第" << ++match_cnt << "次匹配索引"
-                 << "：" << i - lb << endl,
-                j = nxt[j]; //输出每一个匹配的字符串位置只用加上j = nxt[j]即可！！！
+            cout << "第" << ++match_cnt << "次匹配索引：" << i - lb << endl, j = nxt[j]; //输出每一个匹配的字符串位置只用加上j = nxt[j]即可
     }
 }
 
