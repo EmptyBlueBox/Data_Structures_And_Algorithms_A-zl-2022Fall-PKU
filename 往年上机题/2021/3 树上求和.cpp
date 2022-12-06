b6f553a51477fbab2a1e1db118f8ea4b800cf11f
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-12-06 23:05:54 +0800

    Update 3 树上求和.cpp

--- 往年上机题/2021/3 树上求和.cpp
+++ 往年上机题/2021/3 树上求和.cpp
@@ -1,65 +1,59 @@
 #include <iostream>
 using namespace std;
 
-bool has_path = false;
-int goal;
-
-//读取一段可能的空白与这之后的一个字符
-char get_blank_and_char()
+char get_b_c()
 {
-    char c;
-    do
+    char c = getchar();
+    while (c == ' ' || c == '\t' || c == '\n')
         c = getchar();
-    while (c == ' ' || c == '\t' || c == '\n');
     return c;
 }
 
-//默认前面没有空白
-int get_integer()
+int get_i()
 {
-    int positive;
-    char c;
-    //处理负号
-    c = getchar();
-    positive = (c == '-' ? 0 : 1);
-    if (c != '-') //不是负号放回去
-        ungetc(c, stdin);
-    //快读一个数
-    int num = 0;
-    while ((c = getchar()) >= '0' && c <= '9')
-        num = num * 10 + c - '0';
-    ungetc(c, stdin); //最后一定多读了一个才能判断不在数字内
+    char c = getchar();
+    int op;
+    if (c == '-')
+        op = -1;
+    else
+        op = 1, ungetc(c, stdin);
 
-    return positive ? num : -num;
+    int ans = 0;
+    c = getchar();
+    while (c >= '0' && c <= '9')
+        ans = 10 * ans + c - '0', c = getchar();
+
+    ungetc(c, stdin);
+    return ans * op;
 }
 
-bool is_leaf(int path_sum)
+int target;
+bool has_path = false; //即使为true也不要直接返回
+bool empty(int sum)
 {
-    //找到答案也不能立刻返回，要读完所有的数
-    char c;
-    int num;
-
-    get_blank_and_char();                  //读取左括号
-    if ((c = get_blank_and_char()) == ')') //读了所有的空白字符，如果直接是右括号了
+    get_b_c();
+    char c = get_b_c();
+    if (c == ')')
         return true;
-    else                                       //不是右括号那就是数字了
-        ungetc(c, stdin), num = get_integer(); //读取数字
+    else
+        ungetc(c, stdin);
 
-    bool l_is_leaf = is_leaf(path_sum + num);             //看左子树是否只有一对括号
-    bool r_is_leaf = is_leaf(path_sum + num);             //看右子树是否只有一对括号
-    if (l_is_leaf && r_is_leaf && path_sum + num == goal) //左右均是括号且路径和为目标数字
+    int cur = get_i();
+    bool l_empty = empty(sum + cur);
+    bool r_empty = empty(sum + cur);
+    if (l_empty && r_empty && sum + cur == target)
         has_path = true;
 
-    get_blank_and_char(); //读取右括号
-    return false;         //不是只有括号的叶子节点
+    get_b_c();
+    return false;
 }
 
 int main()
 {
-    while (~scanf("%d", &goal)) //当不是-1(EOF)时
+    while (~scanf("%d", &target))
     {
         has_path = false;
-        is_leaf(0);
-        puts(has_path ? "yes" : "no"); // puts会自动补充换行符
+        empty(0);
+        puts(has_path ? "yes" : "no");
     }
 }
